a4771b5289aff13b5016b4b4f9db6c4539bb4994
youngelvis <mokogwue@gmail.com>
2022-08-24 16:37:10 +0100

    second commit- registration login  and reset password pages

---
+++ lib/components/components_for_class_of_varable/category.dart
@@ -0,0 +1,15 @@
+class Category{
+  static const CHURCH = "Church";
+  static const MOSQUE = "Mosque";
+  static const SUPER_MARKET = "Supermarket";
+  static const GYM_HOUSE = "Gym House";
+  static const PHARMACY = "Pharmacy";
+  static const OFFICE = "Office";
+  static const OTHRES = "Others";
+  static const HOSPITAL = "Hospital";
+  static const RESTAURANT = "Restaurant";
+  static const SALOON = "Saloon/Spa";
+  static const ENTERTAINMENT = "Entertainment";
+  static const HOTEL = "Hotel";
+  static const SCHOOL = "School";
+}
