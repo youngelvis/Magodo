b740ae53592c4e674ff8f47f2f3f07960cb0b1ec
youngelvis <mokogwue@gmail.com>
2022-11-01 20:10:57 +0000

    created a form component for DeleteUpdateButtons

---
+++ lib/pages/resident_Page/forms_component/delete_edit_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '/../../components/components_for_class_of_varable/colors.dart' as color;
+import '../../../components/action_page_button2.dart';
+class DeleteUpdateButton extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
+  final onPressedDeleteButton;
+  const DeleteUpdateButton({Key? key, required this.onPressedDeleteButton}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        const Divider(
+          thickness: 2,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            const SizedBox(
+              width: 45,
+            ),
+            ActionPageButton2(
+              onPressed:  onPressedDeleteButton,
+              primaryColor: color.AppColor.decline,
+              text: 'Delete',
+            ),
+            const SizedBox(
+              width: 50,
+            ),
+            ActionPageButton2(
+              onPressed: () {},
+              primaryColor: color.AppColor.verifiedColor,
+              text: 'Update',
+            ),
+
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
